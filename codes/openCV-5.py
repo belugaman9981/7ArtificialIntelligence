ed06c963a958ca5a9b888bbdcd0f1c199f09c3fb
belugaman <littledage@outlook.com>
2025-10-16 21:45:12 -0700

    Update openCV-5.py

--- codes/openCV-5.py
+++ codes/openCV-5.py
@@ -4,4 +4,9 @@ import numpy as np
 print(cv2.__version__)
 
 while True:
-    frame = 
+    frame = np.zeros([250, 250], dtype = np.uint8)
+    cv2.imshow('My Window', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
